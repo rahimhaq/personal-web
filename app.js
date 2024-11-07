039e3ae8db83f66df793baab727203771b0e42d5
Rahim Haq <rahimtahajjadanzahirhaq@gmail.com>
2024-11-07 22:16:29 +0700

    Realtion Table

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.set("view engine", "hbs");
 app.set('views', path.join(__dirname, 'src', 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(
   session({
