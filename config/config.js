605a9b017a1444155ba0d91588b36e6b8e546f64
Rahim Haq <rahimtahajjadanzahirhaq@gmail.com>
2024-11-09 11:27:37 +0700

    Fix deploy- updated

--- config/config.js
+++ config/config.js
@@ -3,28 +3,19 @@ const pg = require("pg"); // Mengimpor pg (PostgreSQL client)
 
 module.exports = {
   "development": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DATABASE,
-    "host": process.env.POSTGRES_HOST,
+    "use_env_variable": "DATABASE_URL",
     "dialect": "postgres",
-    dialectModule : pg,
-    dialectOptions: {
-      ssl: false,
-    },
+    "dialectModule" : pg,
   },
   "production": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DATABASE,
-    "host": process.env.POSTGRES_HOST,
+    "use_env_variable": "DATABASE_URL",
     "dialect": "postgres",
-    dialectModule : pg,
-    dialectOptions: {
+    "dialectModule" : pg,
+    "dialectOptions": {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-  }
-}
+  },
+};
