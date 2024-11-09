0fda0edee1a70df4a376b47f04867e40283613b2
Rahim Haq <rahimtahajjadanzahirhaq@gmail.com>
2024-11-10 00:38:43 +0700

    postinstall

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
-require("dotenv").config(); // Memuat variabel lingkungan dari .env
-const pg = require("pg"); // Mengimpor pg (PostgreSQL client)
+require("dotenv").config();
+const pg = require("pg");
 
 module.exports = {
   "development": {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DATABASE,
-    "host": process.env.POSTGRES_HOST,
+    "host": process.env.POSTGRES_HOST || "localhost",
     "dialect": "postgres",
     dialectModule : pg,
     dialectOptions: {
@@ -26,5 +26,5 @@ module.exports = {
         rejectUnauthorized: false,
       },
     },
-  },
+  }
 }
