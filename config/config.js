3209e2d88787fc0d9bbaf52d5a5d801c8a0eef3b
Rahim Haq <rahimtahajjadanzahirhaq@gmail.com>
2024-11-10 23:34:18 +0700

    add Models

--- config/config.js
+++ config/config.js
@@ -10,7 +10,10 @@ module.exports = {
     dialect: "postgres",
     dialectModule: pg,
     dialectOptions: {
-      ssl: false,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
   production: {
