34792453a80b1b0982dce82b03f828bf0e553562
Rahim Haq <rahimtahajjadanzahirhaq@gmail.com>
2024-11-10 20:30:08 +0700

    Update config.js for Vercel deployment

--- config/config.js
+++ config/config.js
@@ -8,18 +8,15 @@ module.exports = {
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST || "localhost",
     dialect: "postgres",
-    dialectModule : pg,
+    dialectModule: pg,
     dialectOptions: {
       ssl: false,
     },
   },
   production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
-    dialectModule : pg,
+    dialectModule: pg,
     dialectOptions: {
       ssl: {
         require: true,
