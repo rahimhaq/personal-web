0f065d6422c7803ca333ad8ed41c6b1c309727a6
Rahim Haq <rahimtahajjadanzahirhaq@gmail.com>
2025-12-11 13:14:02 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -22,13 +22,24 @@ module.exports = {
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",
-    dialectModule: require("pg"),
+    dialectModule: require("pg"), // Wajib untuk Vercel
+    
+    // --- TAMBAHAN PENTING UNTUK MENGATASI CONNECTION TERMINATED ---
+    pool: {
+      max: 1,       // Maksimal 1 koneksi per instance (PENTING!)
+      min: 0,       // Izinkan koneksi sampai 0 saat sepi
+      acquire: 30000, // Waktu tunggu maksimal 30 detik
+      idle: 10000,  // Putus koneksi jika menganggur 10 detik
+    },
+    // -------------------------------------------------------------
+
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
+        rejectUnauthorized: false, // Wajib false untuk database cloud gratisan
       },
+      keepAlive: true, // Opsional: menjaga koneksi tetap hidup
     },
-    sslmode:"require",
+    sslmode: "require",
   },
 };
